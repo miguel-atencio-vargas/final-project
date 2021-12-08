a7feff73715221f1dcad0fc40144938a018c95cf
skyfall947 <atencio947@gmail.com>
2021-12-08 11:55:00 -0400

    Update user.service JSdoc

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -14,22 +14,36 @@ import { User, UserDocument } from './schemas/user.schema';
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+  /**
+   *
+   * @param createUserDto
+   * @returns a new user created
+   */
   async create(createUserDto: CreateUserDto): Promise<ReadUserDto> {
     try {
       const newUser = new this.userModel(createUserDto);
       const savedUser = await newUser.save();
       return plainToClass(ReadUserDto, savedUser);
     } catch (error) {
-      throw new BadRequestException();
+      throw new BadRequestException(error.message);
     }
   }
 
+  /**
+   *
+   * @param id
+   * @returns a user by ID
+   */
   async findOne(userId: string): Promise<ReadUserDto> {
     const user = await this.userModel.findById(userId);
     if (!user) throw new NotFoundException();
     return plainToClass(ReadUserDto, user);
   }
 
+  /**
+   *
+   * @returns all users in the DB
+   */
   async find(): Promise<ReadUserDto[]> {
     const users = await this.userModel.find();
     if (users.length === 0) throw new NotFoundException();
