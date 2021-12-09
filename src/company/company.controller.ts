2b940e3f6faf349db7648115cc3fa34b12acf7d5
Ernesto <ernesto1991diaz@gmail.com>
2021-12-09 13:28:00 -0600

    Completed CRUD Company

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CompanyService } from './company.service';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { PutCompanyDto } from './dto/put-company.dto';
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+  constructor(private readonly companyService: CompanyService) {}
+  @Post()
+  newCompany(@Body() crateCompanyDto: CreateCompanyDto) {
+    console.log('hello');
+    return this.companyService.create(crateCompanyDto);
+  }
+
+  @Get(':id')
+  getCompanyById(@Param('id') companyId: string) {
+    return this.companyService.findOne(companyId);
+  }
+
+  @Get()
+  getCompanies() {
+    return this.companyService.getAll();
+  }
+
+  @Put(':id')
+  updateCompany(
+    @Param('id') id: string,
+    @Body() patchCompanyDto: PutCompanyDto,
+  ) {
+    return this.companyService.updateOne(id, patchCompanyDto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  removeCompany(@Param('id') id: string) {
+    return this.companyService.removeOne(id);
+  }
+}
