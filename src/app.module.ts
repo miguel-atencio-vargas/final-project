ea3292f60f91bc3d5361ecd0758c1b8bfcecddb0
Ernesto <ernesto1991diaz@gmail.com>
2021-12-09 00:17:41 -0600

    Crated Module Company

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { environments } from './config/enviroments';
+import { CompanyModule } from './company/company.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(environments.mongoUri),
     AuthModule,
+    CompanyModule,
   ],
 })
 export class AppModule {}
