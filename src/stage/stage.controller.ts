34992e9a7b7e80be4bfd4004474f86c8c864212f
Ernesto <ernesto1991diaz@gmail.com>
2021-12-10 18:50:23 -0600

    Created Stage Controller

--- src/stage/stage.controller.ts
+++ src/stage/stage.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateStageDto } from './dto/create-stage.dto';
+import { PutStageDto } from './dto/update-stage.dto';
+import { StageService } from './stage.service';
 
 @Controller('stage')
-export class StageController {}
+export class StageController {
+  constructor(private readonly stageService: StageService) {}
+
+  @Post()
+  newStage(@Body() createStageDto: CreateStageDto) {
+    return this.stageService.create(createStageDto);
+  }
+
+  @Get(':id')
+  getStageById(@Param('id') stageId: string) {
+    return this.stageService.findOne(stageId);
+  }
+
+  @Get()
+  getStages() {
+    return this.stageService.getAll();
+  }
+
+  @Put(':id')
+  updateStage(@Param('id') id: string, @Body() pathStageDto: PutStageDto) {
+    return this.stageService.updateOne(id, pathStageDto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  removeStage(@Param('id') id: string) {
+    return this.stageService.removeOne(id);
+  }
+}
