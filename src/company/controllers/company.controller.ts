1dbbb54477f2aedd362138e3addf205fa42ef61e
skyfall947 <atencio947@gmail.com>
2021-12-11 21:57:25 -0400

    Add auth functionality over controller

--- src/company/controllers/company.controller.ts
+++ src/company/controllers/company.controller.ts
@@ -8,21 +8,29 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
-import { CompanyService } from '../services/company.service';
+import { Roles } from '../../auth/decorator/roles.decorator';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { RoleUser } from '../../users/enum/roles.enums';
 import { CreateCompanyDto } from '../dto/company/create-company.dto';
 import { PutCompanyDto } from '../dto/company/put-company.dto';
+import { CompanyService } from '../services/company.service';
 
 @Controller('companies')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleUser.SUDO_ADMIN)
   @Post()
   newCompany(@Body() crateCompanyDto: CreateCompanyDto) {
     return this.companyService.create(crateCompanyDto);
   }
 
-  @Get(':id')
-  getCompanyById(@Param('id') companyId: string) {
+  @Get(':companyId')
+  getCompanyById(@Param('companyId') companyId: string) {
     return this.companyService.findOne(companyId);
   }
 
@@ -31,17 +39,21 @@ export class CompanyController {
     return this.companyService.getAll();
   }
 
-  @Put(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleUser.SUDO_ADMIN, RoleUser.COMPANY_ADMIN)
+  @Put(':companyId')
   updateCompany(
-    @Param('id') id: string,
+    @Param('companyId') companyId: string,
     @Body() patchCompanyDto: PutCompanyDto,
   ) {
-    return this.companyService.updateOne(id, patchCompanyDto);
+    return this.companyService.updateOne(companyId, patchCompanyDto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(RoleUser.SUDO_ADMIN, RoleUser.COMPANY_ADMIN)
   @HttpCode(HttpStatus.NO_CONTENT)
-  @Delete(':id')
-  removeCompany(@Param('id') id: string) {
-    return this.companyService.removeOne(id);
+  @Delete(':companyId')
+  removeCompany(@Param('companyId') companyId: string) {
+    return this.companyService.removeOne(companyId);
   }
 }
--- src/company/controllers/opening.controller.ts
+++ src/company/controllers/opening.controller.ts
@@ -8,7 +8,12 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
+import { Roles } from '../../auth/decorator/roles.decorator';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+import { RoleUser } from '../../users/enum/roles.enums';
 import { CreateOpeningDto } from '../dto/opening/create-opening.dto';
 import { PutOpeningDto } from '../dto/opening/put-opening.dto';
 import { OpeningService } from '../services/opening.service';
@@ -17,6 +22,12 @@ import { OpeningService } from '../services/opening.service';
 export class OpeningController {
   constructor(private readonly openingService: OpeningService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(
+    RoleUser.SUDO_ADMIN,
+    RoleUser.COMPANY_ADMIN,
+    RoleUser.COMPANY_RECRUITER,
+  )
   @Post()
   newOpening(@Body() createOpeningDto: CreateOpeningDto) {
     return this.openingService.create(createOpeningDto);
