681dd63b2a5a0a5ffb31494146f71c8bdbebc52d
Ernesto <ernesto1991diaz@gmail.com>
2021-12-13 17:07:07 -0600

    Update approved candidate template

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -7,12 +7,10 @@ export class MailService {
 
   async sendConfirmationEmail() {
     await this.mailerService.sendMail({
-      to: 'atencio947@gmail.com',
-      //from: 'foobar@example.com',
+      to: '2551592011@mail.utec.edu.sv',
       subject: 'Welcome to Node Trainee Program',
-      template: './approvedCandidate', // `.hbs` extension is appended automatically
+      template: './approvedCandidate',
       context: {
-        // ✏️ filling curly brackets with content
         name: 'Miguel',
         opening: 'Node js',
       },
@@ -21,27 +19,27 @@ export class MailService {
         {
           filename: 'image-1.png',
           path: __dirname + `/templates/images/image-1.png`,
-          cid: 'twitter', //same cid value as in the html img src
+          cid: 'twitter',
         },
         {
           filename: 'image-2.png',
           path: __dirname + `/templates/images/image-2.png`,
-          cid: 'instagram', //same cid value as in the html img src
+          cid: 'instagram',
         },
         {
           filename: 'image-3.png',
           path: __dirname + `/templates/images/image-3.png`,
-          cid: 'linkeding', //same cid value as in the html img src
+          cid: 'linkeding',
         },
         {
           filename: 'image-4.png',
           path: __dirname + `/templates/images/image-4.png`,
-          cid: 'youtube', //same cid value as in the html img src
+          cid: 'youtube',
         },
         {
           filename: 'image-5.png',
           path: __dirname + `/templates/images/image-5.png`,
-          cid: 'heroImage', //same cid value as in the html img src
+          cid: 'heroImage',
         },
       ],
     });
