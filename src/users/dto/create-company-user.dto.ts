bbbc7c7d02094381e24b712df8d3f01794e941eb
skyfall947 <atencio947@gmail.com>
2021-12-13 01:25:17 -0400

    Update user module to allow creation of recruiters

--- src/users/dto/create-company-user.dto.ts
+++ src/users/dto/create-company-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsEmail, IsEnum, IsOptional } from 'class-validator';
+import { IsString, IsEmail, IsEnum } from 'class-validator';
 import { RoleUser } from '../enum/roles.enums';
 
 export class CreateCompanyUserDto {
@@ -13,8 +13,4 @@ export class CreateCompanyUserDto {
 
   @IsEnum(RoleUser)
   roleEnum: RoleUser;
-
-  @IsString()
-  @IsOptional()
-  companyId?: string;
 }
