6910fc73c3552f042b5a6b9fa2dd0f73069e4f9c
Ernesto <ernesto1991diaz@gmail.com>
2021-12-13 17:29:10 -0600

    Add env example for mail module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -9,16 +9,16 @@ import { MailController } from './mail.controller';
   imports: [
     MailerModule.forRoot({
       transport: {
-        host: 'smtp.gmail.com',
+        host: process.env.MAIL_HOST,
         port: 465,
         secure: true,
         auth: {
-          user: 'ernesto1991diaz@gmail.com',
-          pass: 'gegzcjluilqvatcf',
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
         },
       },
       defaults: {
-        from: 'No Reply <noreply@example.com>',
+        from: process.env.MAIL_FROM,
       },
       template: {
         dir: join(__dirname, 'templates'),
