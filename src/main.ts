49fc73a64d2977872de0651505026ff63c31bae9
Ernesto <ernesto1991diaz@gmail.com>
2021-12-15 10:57:33 -0600

    Update env port

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const host = process.env.HOST || '0.0.0.0';
-  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(port, () => {
-    console.log(`Server up on port ${port}`);
-  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
