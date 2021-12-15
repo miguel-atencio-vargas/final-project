05b9154687a544d4eb031e49883a84c6ec74b9d1
Ernesto <ernesto1991diaz@gmail.com>
2021-12-15 10:51:30 -0600

    Update deployment Port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(port, host, () => {
+  await app.listen(port, () => {
     console.log(`Server up on port ${port}`);
   });
 }
