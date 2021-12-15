da7d059d59505c0bdd1a1d1e1c0db53f79b02825
Ernesto <ernesto1991diaz@gmail.com>
2021-12-15 10:27:53 -0600

    Update dev dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const host = process.env.HOST || '0.0.0.0';
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+
+  await app.listen(port, host, () => {
+    console.log(`Server up on port ${port}`);
+  });
 }
 bootstrap();
