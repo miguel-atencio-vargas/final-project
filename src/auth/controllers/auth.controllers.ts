8265daaf40264f72fc281f172277e4b360d9214c
Ernesto <ernesto1991diaz@gmail.com>
2021-12-17 13:10:40 -0600

    Complete auth swagger documentation

--- src/auth/controllers/auth.controllers.ts
+++ src/auth/controllers/auth.controllers.ts
@@ -9,28 +9,48 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../services/auth.service';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiExcludeEndpoint,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiSecurity,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('authorization')
+@ApiSecurity('Google Token')
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Redirects an user to the Google Login url.' })
+  @ApiResponse({
+    status: 200,
+    description: 'Google auth urld',
+  })
   @Get('google')
   @UseGuards(AuthGuard('google'))
   googleAuth() {
     return;
   }
 
+  @ApiExcludeEndpoint()
   @Get('google/redirect')
   @UseGuards(AuthGuard('google'))
   async googleAuthRedirect(@Req() { user }) {
-    if (!user) throw new NotFoundException('User has not be found');
+    if (!user) throw new NotFoundException('User has not been found');
     const userToken = await this.authService.generateTokenForUser(user);
     if (userToken) return userToken;
     return this.authService.generateTokenForCandidate(user);
   }
 
+  @ApiOperation({ summary: 'Takes an user out of the Api' })
+  @ApiParam({ name: 'jti', description: 'jti to be added to the blacklist' })
+  @ApiResponse({
+    status: 200,
+    description: 'Adds a jti to the banned tokens list',
+  })
   @Post('logout')
   @UseGuards(JwtAuthGuard)
   async logout(@Req() { user }) {
