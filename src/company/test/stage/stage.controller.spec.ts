41733c21d173b3248246e2b86310bd4e6233800e
skyfall947 <atencio947@gmail.com>
2021-12-20 11:12:23 -0400

    Add unit test for stages

--- src/company/test/stage/stage.controller.spec.ts
+++ src/company/test/stage/stage.controller.spec.ts
@@ -1,21 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StageController } from '../../controllers/stage/stage.controller';
+import { ReadStageDto } from '../../dto/stage/read-stage.dto';
 import { StageService } from '../../services/stage.service';
 
 describe('StageController', () => {
   let stageController: StageController;
   let stageService: StageService;
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
       controllers: [StageController],
-      providers: [{ provide: StageService, useValue: {} }],
+      providers: [{ provide: StageService, useValue: { getAll: jest.fn() } }],
     }).compile();
 
-    stageController = module.get<StageController>(StageController);
-    stageService = module.get<StageService>(StageService);
+    stageController = moduleRef.get<StageController>(StageController);
+    console.log('🚀 | beforeEach | stageController', stageController);
+    stageService = moduleRef.get<StageService>(StageService);
   });
 
   it('should be defined', () => {
     expect(stageController).toBeDefined();
+  });
+
+  describe('getStages()', () => {
+    it('should return all stages', () => {
+      jest
+        .spyOn(stageService, 'getAll')
+        .mockResolvedValue([new ReadStageDto()]);
+    });
+    expect(stageController.getStages()).resolves.toHaveLength(1);
+    expect(stageService.getAll).toBeCalledTimes(1);
   });
 });
